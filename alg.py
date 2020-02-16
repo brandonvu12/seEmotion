20a454663333b73c2bd347b2ae35d0700e1ebff9
Brandon Vu <bvu106038@gmail.com>
2020-02-15 20:38:47 -0800

    thresh variable

--- alg.py
+++ alg.py
@@ -6,22 +6,22 @@ def confused(n_tup:tuple):
     for n in n_tup:
         r_tup += (getP(n),)
 
-    bitch = ((3+ sum(r_tup[:3]))/(1+r_tup[-1])) -1
+    thresh = ((3+ sum(r_tup[:3]))/(1+r_tup[-1])) -1
 
-    if bitch >2.5:
+    if thresh >2.5:
         return 100
-    elif bitch >2:
+    elif thresh >2:
         return 80
-    elif bitch > 1.5:
+    elif thresh > 1.5:
         return 60
-    elif bitch > 1:
+    elif thresh > 1:
         return 40
-    elif bitch > 0.5:
+    elif thresh > 0.5:
         return 20
-    elif bitch<=0.5:
+    elif thresh<=0.5:
         return 0
     else:
-        return bitch*100
+        return thresh*100
 
 
 
